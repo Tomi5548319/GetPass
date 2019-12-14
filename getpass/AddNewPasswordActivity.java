8620f27a89e57070dc84172b2ad266a4701c9a37
Tomi5548319 <tomi.oross@gmail.com>
2019-12-14 19:19:25 +0100

    Vymazane zbytocne komentare z AddNewPasswordActivity.java

--- getpass/AddNewPasswordActivity.java
+++ getpass/AddNewPasswordActivity.java
@@ -54,35 +54,6 @@ public class AddNewPasswordActivity extends AppCompatActivity{
 
     private void onCreate(){
 
-        // get the listview
-        //expListView = findViewById(R.id.Advanced_generation_settings);
-
-        // preparing list data
-        // prepareListData();
-
-        // listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-
-        // setting list adapter
-        // expListView.setAdapter(listAdapter);
-
-        /*expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
-            @Override
-            public void onGroupExpand(int i) {
-                TextView test = (TextView) findViewById(R.id.textView_list_item0);
-                test.setVisibility(View.VISIBLE);
-            }
-        });
-
-        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
-            @Override
-            public void onGroupCollapse(int i) {
-                TextView test = (TextView) findViewById(R.id.textView_list_item0);
-                test.setVisibility(View.GONE);
-            }
-        });*/
-
-
-
         // ADVANCED
         mAdvancedHidden = true;
 
@@ -137,70 +108,16 @@ public class AddNewPasswordActivity extends AppCompatActivity{
             public void onClick(View view) {
 
                 mEditTextName = (EditText) findViewById(R.id.editText_add_name);
-                //mEditTextKey = (EditText) findViewById(R.id.editTextKey);
 
                 mName = mEditTextName.getText().toString();
-                //String key = mEditTextKey.getText().toString();
 
                 startSaveNewPasswordActivity(mName, mKey);
 
-                //String returnValue = Password.generate(mName, password);
-                //mTextViewPassword.setText(returnValue);
-
-                        /*mSeed = Password.getSeed();
-                        mFlag = 123; // TODO implement flags*/
+                // TODO implement flags
 
             }
         });
     }
-
-    /*
-     * Preparing the list data
-     */
-    /*private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding header data
-        listDataHeader.add("Advanced");
-        /*listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> adv = new ArrayList<String>();
-        adv.add("Password length");
-        adv.add("a-z ");
-        adv.add("A-Z");
-        adv.add("0-9");
-        adv.add("Basic Symbols        ,.,-?:_" + 0x22 + "!/;*+@()%");
-        adv.add("Advanced Symbols,|" + 0x5c + "€$&^<>~'[]{}");
-        adv.add("Custom characters:");
-        /*top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), adv); // Header, Child data
-        /*listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
-    }*/
 
     private void startSaveNewPasswordActivity(String name, String key){
         Intent intent = new Intent(AddNewPasswordActivity.this, SaveNewPasswordActivity.class);
