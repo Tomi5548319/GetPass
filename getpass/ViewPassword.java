1038a765be9a3b16a9d232a9d73b8b510a892164
Tomi5548319 <tomi.oross@gmail.com>
2019-12-14 16:03:07 +0100

    Vymazane zbytocne cast-y v subore ViewPassword.java

--- getpass/ViewPassword.java
+++ getpass/ViewPassword.java
@@ -1,7 +1,6 @@
 package com.tomi5548319.getpass;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,21 +13,20 @@ public class ViewPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_password);
 
-
-
+        
         if(getIntent().hasExtra("com.tomi5548319.getpass.VIEW_NAME") && getIntent().hasExtra("com.tomi5548319.getpass.VIEW_SEED") && getIntent().hasExtra("com.tomi5548319.getpass.VIEW_KEY")) {
-            String name = (String) getIntent().getExtras().getString("com.tomi5548319.getpass.VIEW_NAME");
-            String seed = (String) getIntent().getExtras().getString("com.tomi5548319.getpass.VIEW_SEED");
-            String key = (String) getIntent().getExtras().getString("com.tomi5548319.getpass.VIEW_KEY");
+            String name = getIntent().getExtras().getString("com.tomi5548319.getpass.VIEW_NAME");
+            String seed = getIntent().getExtras().getString("com.tomi5548319.getpass.VIEW_SEED");
+            String key = getIntent().getExtras().getString("com.tomi5548319.getpass.VIEW_KEY");
 
-            final TextView textViewFinalPassword = (TextView) findViewById(R.id.textView_view_key);
+            final TextView textViewFinalPassword = findViewById(R.id.textView_view_key);
 
             final String password = (Password.generate(name, key, seed));
 
             textViewFinalPassword.setText("*****");
 
-            final Button buttonShow = (Button) findViewById(R.id.button_view_show);
-            final Button buttonHide = (Button) findViewById(R.id.button_view_hide);
+            final Button buttonShow = findViewById(R.id.button_view_show);
+            final Button buttonHide = findViewById(R.id.button_view_hide);
 
             buttonShow.setOnClickListener(new View.OnClickListener() {
                 @Override
